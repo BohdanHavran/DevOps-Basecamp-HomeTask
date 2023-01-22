0b63b7eadaf93fc8370bb03361bba89635811c66
BohdanHavran <danohavran@gmail.com>
2023-01-22 07:02:17 +0000

    Jenkins

--- task9/Deploy/main.tf
+++ task9/Deploy/main.tf
@@ -1,5 +1,5 @@
 module "AWS_Grafana" {
-  source = "github.com/BohdanHavran/DevOps-Basecamp-HomeTask/tree/main/task6/modules/AWS"
+  source = "github.com/BohdanHavran/DevOps-Basecamp-HomeTask/task6/modules/AWS"
   # AWS Instance configurations --------------------------------------------------
   # instance_type = "t2.micro"
   # key_name      = "id_rsa"
